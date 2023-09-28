9d6f07975fb25da9a998522619126c52ce3d54a4
Nourezzehi <nour.ezzhi.insat.ucar.tn>
2023-09-28 11:36:47 +0100

    copy

--- python-hello_world/7-edges.py
+++ python-hello_world/7-edges.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 word = "Holberton"
-word_first_3 = word[:4]
+word_first_3 = word[:3]
 word_last_2, middle_word = word[-2:], word[1:-1]
 print(f"First 3 letters: {word_first_3}")
 print(f"Last 2 letters: {word_last_2}")
