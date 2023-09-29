199dad33e90b79676839c854fe985b6644ae5379
Nourezzehi <nour.ezzhi.insat.ucar.tn>
2023-09-29 23:52:23 +0100

    hidden

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    i = dir(hidden_4)
-    for i in names:
-        if i[:2] != "__":
-            print(i)
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
