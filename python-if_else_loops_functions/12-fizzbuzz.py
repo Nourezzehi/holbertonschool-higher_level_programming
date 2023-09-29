4062b82fe85f318d322393a3d12402e81fc09767
Nourezzehi <nour.ezzhi.insat.ucar.tn>
2023-09-29 22:02:04 +0100

    fizzbuz

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -8,5 +8,5 @@ def fizzbuzz():
         elif (i % 3 != 0 and i % 5 == 0):
             print("Buzz", end=" ")
         else:
-            print(i, end=" ")
+            print("{:d}".format(i), end=" ")
     print("Buzz", end=" ")
