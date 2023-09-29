8875653d51889a0ae52023d7461b80cd383f31ba
Nourezzehi <nour.ezzhi.insat.ucar.tn>
2023-09-29 21:49:46 +0100

    l.digit

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,5 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 def print_last_digit(number):
-    if number < 0:
-        number *= -1
-    print(number % 10, end="")
-    return number % 10
+    number = abs(number) % 10
+    print(number, end="")
+    return number
