7dc200e4db0a571447cfd98c138e545051834687
Nourezzehi <nour.ezzhi.insat.ucar.tn>
2023-10-04 23:30:07 +0100

    sen and f_char

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if sentence is None:
-        return ()
+        return (0, "")
     f_c = sentence[0]
     if sentence == "":
         f_c = None
