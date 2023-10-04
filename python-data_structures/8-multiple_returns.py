d4257879d7ca5b4aff14a8e7f41b95db5ef58dd1
Nourezzehi <nour.ezzhi.insat.ucar.tn>
2023-10-04 23:34:16 +0100

    output error

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -3,6 +3,4 @@ def multiple_returns(sentence):
     if sentence is None:
         return (0, None)
     f_c = sentence[0]
-    if sentence == "":
-        f_c = None
     return (len(sentence), f_c)
