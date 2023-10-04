3288cbfa4d2ddb8a1e4b0a4928c28e0fa3c3f512
Nourezzehi <nour.ezzhi.insat.ucar.tn>
2023-10-04 23:33:02 +0100

    sen and f_char

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if sentence is None:
-        return (0, "")
+        return (0, None)
     f_c = sentence[0]
     if sentence == "":
         f_c = None
