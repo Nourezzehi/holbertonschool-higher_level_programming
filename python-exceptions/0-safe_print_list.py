a0630f05a96a6af5a25219bfb77488171b9dc20d
Nourezzehi <nour.ezzhi.insat.ucar.tn>
2023-10-06 19:11:11 +0100

    loopchange

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,8 +3,8 @@ def safe_print_list(my_list=[], x=0):
     i = 0
     item = ""
     try:
-        for item in my_list:
-            print(item, end='')
+        for i in range(0, x):
+            print("{}".format(my_list[i]), end='')
             i += 1
             if i == x:
                 break
