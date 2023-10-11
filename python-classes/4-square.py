6383f07fcacc0ec51093cf77f47aecdfcb5f96a5
Nourezzehi <nour.ezzhi.insat.ucar.tn>
2023-10-11 12:11:03 +0100

    setter and getter fixed

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -6,17 +6,16 @@ class Square:
     """square class"""
 
     def __init__(self, size=0):
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
+        """initialize with a given size"""
         self.__size = size
 
+    @property
     def size(self):
         """getter"""
 
         return self.__size
 
+    @size.setter
     def size(self, value):
         """setter"""
 
