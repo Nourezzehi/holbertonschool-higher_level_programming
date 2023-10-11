643550267f020e62e570a4ea22e1a0db31c2c7b9
Nourezzehi <nour.ezzhi.insat.ucar.tn>
2023-10-11 10:02:53 +0100

    getter_setter

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-"""getter method"""
+"""getter_setter method """
 
 
 class Square:
     """square class"""
 
     def __init__(self, size=0):
-        """if not isinstance(size, int):
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")"""
+        if size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
 
     def size(self):
@@ -22,7 +22,7 @@ class Square:
 
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
