8b51503de8c677191e3a606adf2bff60e8b90fa2
Nourezzehi <nour.ezzhi.insat.ucar.tn>
2023-10-19 22:05:31 +0100

    isubclass and not same

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -5,4 +5,5 @@
 def inherits_from(obj, a_class):
     """ returns True if the object is an instance of a class that inherited
     (directly or indirectly) from the specified class ; otherwise False"""
-    return issubclass(type(obj), a_class)
+    if type(obj) != a_class:
+        return issubclass(type(obj), a_class)
