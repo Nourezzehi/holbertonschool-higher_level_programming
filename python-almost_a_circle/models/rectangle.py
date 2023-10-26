d2c1b82b5a5f7c2d4a0d2a9ca4cd383a3ae22196
Nourezzehi <nour.ezzhi.insat.ucar.tn>
2023-10-26 20:00:43 +0100

    rectangle class

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+"""Rectangle class"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """inherits of base class"""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """instantiation"""
+
+        id = super().__init__(id)
+        self.x = x
+        self.y = y
+        self.height = height
+        self.width = width
+
+    @property
+    def x(self):
+        """x-get_method"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """x-set_method"""
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @property
+    def y(self):
+        """y-get_method"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """y-set_method"""
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
+
+    @property
+    def height(self):
+        """height-get_method"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """height-get_method"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
+    @property
+    def width(self):
+        """width-get_method"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """width-set_method"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
