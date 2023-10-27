3df6f0e7f84772e3f883fc4e3d9720d86c8a4f32
Nourezzehi <nour.ezzhi.insat.ucar.tn>
2023-10-27 14:35:56 +0100

    square class

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -13,5 +13,6 @@ class Square(Rectangle):
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
+        """string representation"""
         return "[Square] ({}) {}/{} - {}".format(
             self.id, self.x, self.y, self.width)
