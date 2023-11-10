44449274595ca3e6a2837c3f26e73abe2f90f5ee
Nourezzehi <nour.ezzhi.insat.ucar.tn>
2023-11-10 17:04:49 +0100

    loop changed

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -10,6 +10,8 @@ def print_square(size):
     if size < 0:
         raise ValueError("size must be >= 0")
     if isinstance(size, float) and size < 0:
-        raise TypeError('size must be an integer')
+        raise TypeError("size must be an integer")
     for i in range(size):
-        print("#" * size)
+        for j in range(size):
+            print('#', end='')
+        print()
