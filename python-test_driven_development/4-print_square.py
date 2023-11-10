807648e71079c585d423d73297b07c7d59841b4b
Nourezzehi <nour.ezzhi.insat.ucar.tn>
2023-11-10 16:58:49 +0100

    condition fixed

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -4,8 +4,6 @@
 
 def print_square(size):
     """prints a square with the character '#' """
-    if type(size) is float and size < 0:
-        raise TypeError("size must be an integer")
     if type(size) is not int:
         raise TypeError("size must be an integer")
     if size < 0:
