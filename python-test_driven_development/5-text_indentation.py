dc05c73764f9014134260774c63123e64736dbc1
Nourezzehi <nour.ezzhi.insat.ucar.tn>
2023-11-10 19:36:13 +0100

    output error

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -4,7 +4,7 @@
 
 def text_indentation(text):
     """prints a text with 2 new lines after each of these characters:
-    ., ? and :"""
+        ., ? and :"""
 
     if type(text) is not str:
         raise TypeError("text must be a string")
@@ -13,7 +13,6 @@ def text_indentation(text):
         if text[i] in [':', '.', '?']:
             print(text[i])
             print()
-            i += 1
         else:
             print(text[i], end='')
         i += 1
