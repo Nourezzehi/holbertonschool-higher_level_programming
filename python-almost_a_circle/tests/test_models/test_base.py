4dbc0ee6eb574d24482e17e7c07f071c07d88f05
Nourezzehi <nour.ezzhi.insat.ucar.tn>
2023-11-22 19:39:11 +0100

    to_json method unittest

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -2,7 +2,8 @@
 """unittest for base.py"""
 
 import unittest
-from models.base import Base 
+from models.base import Base
+import json
 
 class TestBaseClass(unittest.TestCase):
     def test_instantiation_with_id_none(self):
@@ -18,6 +19,20 @@ class TestBaseClass(unittest.TestCase):
         obj2 = Base()
         self.assertEqual(obj1.id, 1)
         self.assertEqual(obj2.id, 2)
+
+    def test_to_json_string_empty_list(self):
+        result = Base.to_json_string([])
+        self.assertEqual(result, "[]")
+
+    def test_to_json_string_none(self):
+        result = Base.to_json_string(None)
+        self.assertEqual(result, "[]")
+
+    def test_to_json_string_valid_input(self):
+        input_data = [{'key': 'value'}, {'another_key': 'another_value'}]
+        result = Base.to_json_string(input_data)
+        expected_result = json.dumps(input_data)
+        self.assertEqual(result, expected_result)
 
 if __name__ == '__main__':
     unittest.main()
