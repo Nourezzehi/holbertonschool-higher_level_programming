c4fb13ae9b4acd2fdb0e5df26d6337a9dc837dec
Nourezzehi <nour.ezzhi.insat.ucar.tn>
2023-11-23 11:45:17 +0100

    check

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -11,8 +11,6 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """constructor"""
         super().__init__(size, size, x, y, id)
-        self.size = size
-
     def __str__(self):
         """ return the representation of a square """
         return "[Square] ({}) {}/{} - {}".\
