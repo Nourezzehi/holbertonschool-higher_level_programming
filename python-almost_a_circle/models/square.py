aa73f96b288acdb3e41d5d1b47da61a9cf2f7c1c
Nourezzehi <nour.ezzhi.insat.ucar.tn>
2023-11-23 11:39:29 +0100

    setter fix

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -11,7 +11,7 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """constructor"""
         super().__init__(size, size, x, y, id)
-
+        self.s
     def __str__(self):
         """ return the representation of a square """
         return "[Square] ({}) {}/{} - {}".\
