e77010226a0b8dc52b695b47aeb1970eb0695505
Nourezzehi <nour.ezzhi.insat.ucar.tn>
2023-11-23 10:55:41 +0100

    constructor fix

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -11,6 +11,7 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """constructor"""
         super().__init__(size, size, x, y, id)
+        self.size = size
 
     def __str__(self):
         """string represenation of the square"""
