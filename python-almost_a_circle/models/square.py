a58ff49a3179444234f705b07b4721661f83011b
Nourezzehi <nour.ezzhi.insat.ucar.tn>
2023-11-23 11:33:24 +0100

    setter fix

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -26,6 +26,10 @@ class Square(Rectangle):
     @size.setter
     def size(self, value):
         """set_method"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
         self.width = value
         self.height = value
 
