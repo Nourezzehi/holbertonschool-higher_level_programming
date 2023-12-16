2111836d196c589cc2d66c53869f5c8c0f3eda89
Nourezzehi <nour.ezzhi.insat.ucar.tn>
2023-12-17 00:05:25 +0100

    fix error

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -32,7 +32,7 @@ class Rectangle(Base):
     @y.setter
     def y(self, value):
         """y-set_method"""
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("y must be an integer")
         if value < 0:
             raise ValueError("y must be >= 0")
@@ -46,7 +46,7 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         """height-get_method"""
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("height must be an integer")
         if value <= 0:
             raise ValueError("height must be > 0")
@@ -60,7 +60,7 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         """width-set_method"""
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
