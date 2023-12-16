64f1514d507e337f367a052a46b9952ede1f3f36
Nourezzehi <nour.ezzhi.insat.ucar.tn>
2023-12-17 00:13:46 +0100

    conditions added

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -22,6 +22,10 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, value):
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
